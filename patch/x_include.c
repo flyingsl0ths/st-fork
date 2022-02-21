944a9922320e2296c162b376555f22629ea0cbed
flyingsl0ths <randomemail32110@gmail.com>
2022-02-21 12:15:15 -0600

    Add project files

---
+++ patch/x_include.c
@@ -0,0 +1,5 @@
+/* Patches */
+#include "boxdraw.c"
+#include "font2.c"
+#include "rightclicktoplumb_x.c"
+#include "xresources.c"
---
+++ patch/x_include.h
@@ -0,0 +1,5 @@
+/* Patches */
+#include "boxdraw.h"
+#include "font2.h"
+#include "rightclicktoplumb_x.h"
+#include "xresources.h"
